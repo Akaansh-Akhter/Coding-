c2b43f4ecdb30597473c9bfd13e10ed41bf995ae
Akaansh-Akhter <91464172+Akaansh-Akhter@users.noreply.github.com>
2021-11-19 14:56:19 +0400

    Add files via upload

---
+++ Class25.js
@@ -0,0 +1,29 @@
+function click_me(){
+    alert("Random");
+}
+function click_me2(){
+    var a=4321;
+    var b=1234;
+    alert("The sum of the numbers is="+(a+b));
+}
+function click_me3(){
+    var a=12;
+    var b=13;
+    var c=46;
+    var d=3;
+    var e=8;
+    var f=7;
+    alert("The sum of the numbers is="+(a*b-(c-d)+e/f));
+}
+function click_me4(){
+    var a=12;
+    alert("12X12="+(a*a));
+}
+function click_me5(){
+    var a=12;
+    alert("12+12="+(a+a));
+}
+function showState(){
+    var a=document.getElementById("id1").value;
+    alert(" You have selected "+a);
+}
